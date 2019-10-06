871971c14f0b59eed6d4fed1945f3e42d5f100c3
zhouz <523612913@qq.com>
2019-10-06 11:14:36 +0800

    1.2.1

--- src/test.php
+++ src/test.php
@@ -1,4 +1,21 @@
 <?php
 
-echo 'ver1.2.1';
+class TestA {
+    public function test()
+    {
+        echo 'TestA - test';
+    }
+}
 
+
+$a = 'aaaaaaaaaaaaa';
+$b = 'bbbbbbbbbbbbbb';
+$c = 'ccccccccccccc';
+
+$d = $a . $b. $c;
+
+echo 'ver1.2.1';
+echo $d;
+
+$ta = new TestA();
+$ta->test();
